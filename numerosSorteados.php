f6df7dbbbc9054be1039a69cca4ba77501194b15
Marcelo Moura <marcelo.moura1511@gmail.com>
2021-07-22 02:29:08 -0300

    numeros sorteados

---
+++ numerosSorteados.php
@@ -0,0 +1,19 @@
+<?php
+ 
+    $array=[];
+    for($i=0; $i<20; $i++){
+        $array[] = random_int(1, 10);
+    }
+    echo "Array sorteado = [".implode(",", $array)."]<br>";
+    $countArray = array_count_values($array);
+    $maxVal = max($countArray);
+    $maxValKeys = array_keys($countArray, $maxVal);
+    asort($maxValKeys);
+    echo "O(s) número(s) que mais se repete: " . implode(", ", $maxValKeys);
+    // foreach ($maxValKeys as $num) {
+    //     echo $num."; ";
+    // }
+    echo "<br>Se repete: $maxVal vezes";
+
+
+?>
